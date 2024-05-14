5aa8f3766fbbbb8b6f871d8dd9608f87bcd76025
Alexandr <80054269+AlexandrPashinin@users.noreply.github.com>
2024-05-14 17:09:14 +0300

    fix

--- components/UnitLogic.js
+++ components/UnitLogic.js
@@ -14,7 +14,7 @@ export function createArmies(sceneRef) {
 
     const loader = new GLTFLoader();
     loader.load(
-        '/public/Horse.glb',
+        '/Horse.glb',
         (gltf) => {
             model = gltf.scene;
             mixer = new THREE.AnimationMixer(model);
